3fdcdf66ad52b604232d9b9872615af727a04df0
Seb Martin <seb.d.martin@gmail.com>
2016-03-27 22:46:24 -0400

    Add test app to make Target/Action APIs testable

---
+++ TestApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestApp
+//
+//  Created by Seb Martin on 2016-03-27.
+//  Copyright © 2016 Seb Martin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  // This app exists for the sole purpose of running as the Unit Test host app.  Without this app, any unit tests
+  // using Cocoa's Target/Action API will fail.
+  func applicationDidFinishLaunching(application: UIApplication) {
+    
+  }
+
+}
+
